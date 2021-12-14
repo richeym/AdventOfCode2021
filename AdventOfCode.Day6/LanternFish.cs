8ec719c40d09a60da12487dbca61d77bbc67dcaa
Rick Mather <rick.mather@ao.com>
2021-12-14 10:20:41 +0000

    Day 6 part 2 solution

--- AdventOfCode.Day6/LanternFish.cs
+++ AdventOfCode.Day6/LanternFish.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode.Day6
         }
 
         [Fact]
-        public void Test1()
+        public void Test1_BruteForce()
         {
             var data = FileUtil.ParseRecords<string>("input.txt").ToList();
             var lanternFish = data.First().Split(',').Select(int.Parse).ToList();
@@ -25,7 +25,7 @@ namespace AdventOfCode.Day6
             for (int day = 0; day < 80; day++)
             {
                 var totalFish = lanternFish.Count;
-                for(int fish = 0; fish < totalFish; fish++)
+                for (int fish = 0; fish < totalFish; fish++)
                 {
                     if (lanternFish[fish] == 0)
                     {
@@ -38,8 +38,31 @@ namespace AdventOfCode.Day6
                     }
                 }
             }
-            
+
             _testOutputHelper.WriteLine($"{lanternFish.Count}");
+        }
+
+        [Fact]
+        public void Test2_Elegant()
+        {
+            var data = FileUtil.ParseRecords<string>("input.txt").ToList();
+            var lanternFish = data.First().Split(',').Select(int.Parse).ToList();
+            var fishPerDayCycle = new long[9];
+
+            for (int i = 1; i <= 5; i++)
+            {
+                fishPerDayCycle[i] = lanternFish.Count(x => x == i);
+            }
+            
+            for (var i = 0; i < 256; i++)
+            {
+                long day0 = fishPerDayCycle[0];
+                Array.Copy(fishPerDayCycle, 1, fishPerDayCycle, 0, fishPerDayCycle.Length - 1);
+                fishPerDayCycle[8] = day0;
+                fishPerDayCycle[6] += day0;
+            }
+            
+            _testOutputHelper.WriteLine($"{fishPerDayCycle.Sum()}");
         }
     }
 }
