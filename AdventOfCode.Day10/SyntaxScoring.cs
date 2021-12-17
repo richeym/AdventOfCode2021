6e36f1591d2a6ccee69cf3730cc902a660e8d318
Rick Mather <rick.mather@ao.com>
2021-12-17 13:31:04 +0000

    Day 10 part 2

--- AdventOfCode.Day10/SyntaxScoring.cs
+++ AdventOfCode.Day10/SyntaxScoring.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Util;
@@ -13,6 +12,7 @@ namespace AdventOfCode.Day10
         private readonly List<char> _openingOperators = new() { '(', '[', '{', '<', };
         private readonly List<char> _closingOperators = new() { ')', ']', '}', '>', };
         private readonly List<int> _illegalCharScores = new() { 3, 57, 1197, 25137 };
+        private readonly List<int> _incompleteCharScores = new() { 1, 2, 3, 4 };
 
         public SyntaxScoring(ITestOutputHelper testOutputHelper)
         {
@@ -25,12 +25,14 @@ namespace AdventOfCode.Day10
             var data = FileUtil.ParseRecords<string>("input.txt");
 
             var firstIllegalChars = new List<char>();
-            
+            var incompleteSequenceScores = new List<long>();
+
             foreach (var record in data)
             {
                 var stack = new Stack<char>();
-                
-                foreach (var op in record)
+                var validInput = true;
+
+                foreach (var op in record.TakeWhile(_ => validInput))
                 {
                     if (_openingOperators.Contains(op))
                     {
@@ -39,17 +41,39 @@ namespace AdventOfCode.Day10
                     else
                     {
                         var expectedOpeningOperator = _openingOperators[_closingOperators.IndexOf(op)];
+                    
                         if (expectedOpeningOperator != stack.Pop())
                         {
                             firstIllegalChars.Add(op);
-                            break;
+                            validInput = false;
                         }
                     }
                 }
+
+                if (validInput)
+                {
+                    long score = 0;
+                    
+                    while (stack.Any())
+                    {
+                        var unclosedTag = stack.Pop();
+                        var matchedScore = _incompleteCharScores[_openingOperators.IndexOf(unclosedTag)];
+                        score = score * 5 + matchedScore;
+                    }
+
+                    incompleteSequenceScores.Add(score);
+                }
             }
 
-            int errorSum = firstIllegalChars.Sum(x => _illegalCharScores[_closingOperators.IndexOf(x)]);
-            _testOutputHelper.WriteLine(errorSum.ToString());
+            var errorSum = firstIllegalChars.Sum(x => _illegalCharScores[_closingOperators.IndexOf(x)]);
+            var middleScore = incompleteSequenceScores
+                .OrderBy(x => x)
+                .Skip(incompleteSequenceScores.Count / 2)
+                .Take(1)
+                .Single();
+            
+            _testOutputHelper.WriteLine($"Error sum: {errorSum.ToString()}");
+            _testOutputHelper.WriteLine($"Middle score: {middleScore.ToString()}");
         }
     }
 }
